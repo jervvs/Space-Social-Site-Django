eef86271300383f51813eb07238d808ea11d4095
Jervis <jerv0003@e.ntu.edu.sg>
2020-05-29 23:25:27 +0800

    basic setup

---
+++ newenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jervis/Documents/GitHub/Space-Social-Site-Django/newenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
